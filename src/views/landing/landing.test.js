711b2f8262045837b4b02e6aab0fbf54cab2ac6f
Ryan Chase <ryan@chasery.com>
2021-01-20 10:29:34 -0800

    Missed test update

--- src/views/landing/landing.test.js
+++ src/views/landing/landing.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
-import Landing from "./landing";
+import Landing from "./Landing";
 
 describe("Landing component", () => {
   it("renders without error", () => {
