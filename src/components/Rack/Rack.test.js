70a3be6960e171939a140142a031b731328334f4
Ryan Chase <ryan@chasery.com>
2021-01-23 13:26:35 -0800

    Fix tests for rack and view rack components

--- src/components/Rack/Rack.test.js
+++ src/components/Rack/Rack.test.js
@@ -3,6 +3,14 @@ import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 import Rack from "./Rack";
 
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"), // use actual for all non-hook parts
+  useParams: () => ({
+    rackId: "",
+  }),
+  useRouteMatch: () => ({ url: "/company/company-id1/team/team-id1" }),
+}));
+
 describe("Rack component", () => {
   it("renders without error", () => {
     const wrapper = shallow(<Rack />);
--- src/views/ViewRack/ViewRack.test.js
+++ src/views/ViewRack/ViewRack.test.js
@@ -3,6 +3,14 @@ import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 import ViewRack from "./ViewRack";
 
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"), // use actual for all non-hook parts
+  useParams: () => ({
+    rackId: "1",
+  }),
+  useRouteMatch: () => ({ url: "/company/company-id1/team/team-id1" }),
+}));
+
 describe("ViewRack component", () => {
   it("renders without error", () => {
     const wrapper = shallow(<ViewRack />);
