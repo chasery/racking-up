cbbf6d6a602d9a6b17ba92c1d4c007dc4b671281
Ryan Chase <ryan@chasery.com>
2021-01-24 12:04:59 -0800

    Refactor the edit rack item form to be controlled

--- src/views/EditRackItem/EditRackItem.js
+++ src/views/EditRackItem/EditRackItem.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "../../components/Form/Form";
 import FormField from "../../components/FormField/FormField";
 import "./EditRackItem.css";
 
 function EditRackItem(props) {
+  const [name, setName] = useState("");
+  const [url, setUrl] = useState("");
+  const [price, setPrice] = useState("");
+
   return (
     <main role="main">
       <section className="EditRackItem">
@@ -11,9 +15,30 @@ function EditRackItem(props) {
           <Form id="EditRackItem">
             <h2>Edit Rack Item</h2>
             <p>Edit the information for the clothing item.</p>
-            <FormField id="name" label="Name" type="text" isRequired={true} />
-            <FormField id="url" label="URL" type="text" isRequired={true} />
-            <FormField id="price" label="Price" type="text" isRequired={true} />
+            <FormField
+              id="name"
+              label="Name"
+              type="text"
+              isRequired={true}
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
+            <FormField
+              id="url"
+              label="URL"
+              type="text"
+              isRequired={true}
+              onChange={(e) => setUrl(e.target.value)}
+              value={url}
+            />
+            <FormField
+              id="price"
+              label="Price"
+              type="text"
+              isRequired={true}
+              onChange={(e) => setPrice(e.target.value)}
+              value={price}
+            />
             <button>Cancel</button>
             <button>Edit Rack Item</button>
           </Form>
